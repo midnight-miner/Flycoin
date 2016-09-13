8e79d7d5ea479271aef426e752ec391962b58d7e
Greg Griffith <tfa.productions.1337@gmail.com>
2016-09-13 01:30:09 -0700

    added more functionality to the masternodes including initial setup
    added more functionality to the masternodes including initial setup

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -1,11 +1,95 @@
 #include "masternode.h"
 #include "base58.h"
+#include "init.h"
 
-CMasterNode* myMasterNode;
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#ifdef WIN32
+#include <windows.h>
+#include <wininet.h>
+#else
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif
 
-CMasterNode::CMasterNode()
+CNode* myNode = NULL;
+CMasterNode* myMasterNode = NULL;
+MasterNodeControl* pMasterNodeMain = NULL;
+
+void createMyNode()
 {
+    ///this sets our own local stats for a CNode
 
+    CNetAddr addrLocalHost;
+    if (GetMyExternalIP(addrLocalHost))
+    {
+        printf("GetMyExternalIP() returned %s\n", addrLocalHost.ToStringIP().c_str());
+    }
+    myNode = new CNode(INVALID_SOCKET, CAddress(CService(addrLocalHost, GetListenPort()), nLocalServices));
+}
+
+bool setupFlyNetwork()
+{
+    createMyNode();
+    pMasterNodeMain = new MasterNodeControl();
+    ///check to see if we have enough to be a masternode
+    int64_t ourBalance = pwalletMain->GetBalance();
+    printf("Our balance is %" PRId64 " satoshi \n", ourBalance);
+    int64_t minimumForMaster = 500 * COIN;
+    printf("Minimum balance for masternode is %" PRId64 " satoshi \n", minimumForMaster);
+    if(ourBalance > minimumForMaster)
+    {
+        printf("We are broadcasting as a master node \n");
+        /// only setup our masterNode if we qualify to run as a masterNode
+        myMasterNode = new CMasterNode(*myNode);
+        //Get all our addresses with a balance and add them to our masternodes list of addresses
+        BOOST_FOREACH(set<CTxDestination> grouping, pwalletMain->GetAddressGroupings())
+        {
+            BOOST_FOREACH(CTxDestination address, grouping)
+            {
+                myMasterNode->addAddressToList(CBitcoinAddress(address).ToString());
+            }
+        }
+    }
+    return true;
+}
+//SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn = "", bool fInboundIn=false) : vSend(SER_NETWORK, MIN_PROTO_VERSION), vRecv(SER_NETWORK, MIN_PROTO_VERSION
+CMasterNode::CMasterNode(const CNode& node) : CNode(node.hSocket, node.addr, node.addrName, node.fInbound)
+{
+    masterNodeAddresses.clear();
+    lastReceived = 0;
+
+    nServices = node.nServices;
+    nLastSend = node.nLastSend;
+    nLastRecv = node.nLastRecv;
+    nLastSendEmpty = node.nLastSendEmpty;
+    nTimeConnected = node.nTimeConnected;
+    nHeaderStart = node.nHeaderStart;
+    nMessageStart = node.nMessageStart;
+    nVersion = node.nVersion;
+    strSubVer = node.strSubVer;
+    fOneShot = node.fOneShot;
+    fClient = node.fClient; // set by version message
+    fNetworkNode = node.fNetworkNode;
+    fSuccessfullyConnected = node.fSuccessfullyConnected;
+    fDisconnect = node.fDisconnect;
+    nRefCount = node.nRefCount;
+    nReleaseTime = node.nReleaseTime;
+    hashContinue = node.hashContinue;
+    pindexLastGetBlocksBegin = node.pindexLastGetBlocksBegin;
+    hashLastGetBlocksEnd = node.hashLastGetBlocksEnd;
+    nStartingHeight = node.nStartingHeight;
+    fGetAddr = node.fGetAddr;
+    nMisbehavior = node.getMisbehavior();
+    hashCheckpointKnown = node.hashCheckpointKnown;
+    setInventoryKnown.max_size(SendBufferSize() / 1000);
+    setInventoryKnown = node.setInventoryKnown;
+}
+
+void CMasterNode::addAddressToList(std::string addr)
+{
+    masterNodeAddresses.push_back(addr);
 }
 
 // this checks to see if the node is me
@@ -20,10 +104,20 @@ bool CMasterNode::IsMe()
     }
 }
 
+void CMasterNode::updateLastTime()
+{
+
+}
+
 
 MasterNodeControl::MasterNodeControl()
 {
     this->masterNodeList.clear();
+}
+
+bool MasterNodeControl::updateMasterNodeList()
+{
+
 }
 
 std::vector<CMasterNode*> MasterNodeControl::getMasterNodeList()
@@ -46,7 +140,7 @@ CMasterNode* MasterNodeControl::selectRandomMasterNode()
         //cant send coins to myself for a masternode reward
         goto begin; /// you lovin that goto swag arent u? yeah. uk u are
     }
-    if(selectedNode->nMisbehavior > 10)
+    if(selectedNode->getMisbehavior() > 10)
     {
         //select a new node if the selected one is being naughty.
         goto begin;
--- src/masternode.h
+++ src/masternode.h
@@ -5,19 +5,29 @@
 #include <vector>
 #include "net.h"
 
-extern CMasterNode myMasterNode;
+class CMasterNode;
+class MasterNodeControl;
+
+extern CNode* myNode;
+extern CMasterNode* myMasterNode;
+extern MasterNodeControl* pMasterNodeMain;
+extern bool setupFlyNetwork();
+extern void createMyNode();
+
 
 /// The MasterNode is an extension off of a regular CNode peer that keeps the info needed for syncing masternode lists among the network
 /// Our MasterNode information is held in the myMasterNode variable;
 class CMasterNode : public CNode
-{
+{    
 private:
     std::vector<std::string> masterNodeAddresses; // the list of all addresses in a wallet that have a balance that counts towards masternodes, default is all
     int64_t lastReceived; /// the last time a payment was received for this master node
 
 public:
-    CMasterNode();
+    CMasterNode(const CNode &node);
+    void addAddressToList(std::string addr);
     bool IsMe();
+    void updateLastTime();
 };
 
 
@@ -32,6 +42,7 @@ private:
 
 public:
     MasterNodeControl();
+    bool updateMasterNodeList();
     std::vector<CMasterNode*> getMasterNodeList();
     CMasterNode* selectRandomMasterNode();
 
